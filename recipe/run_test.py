dd483a8a16e26f58b2c1c4dabb624690aa6489dc
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-01-31 07:59:18 -0600

    update deps, python 3.14t, use pixi, try abi3audit

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,7 +1,18 @@
 import sys
 from subprocess import call
+import site
+from pathlib import Path
+from typing import Any
+
+try:
+    __import__("abi3audit")
+    IS_ABI3 = True
+except ImportError:
+    IS_ABI3 = False
+
 
 WIN = sys.platform == "win32"
+SP_DIR = Path(site.getsitepackages()[-1])
 
 FAIL_UNDER = "81"
 COV = ["coverage"]
@@ -24,10 +35,27 @@ SKIP_OR = " or ".join(SKIPS)
 K = ["-k", f"not ({SKIP_OR})"]
 
 
+def do(*args: Any) -> int:
+    """Condition and print arguemnts, return rc."""
+    args = [map(str, args)]
+    print(">>>", *args, flush=True)
+    return call(args)
+
+
+def check_abi(python_min: str) -> int:
+    """Maybe run abi3audit."""
+    if not IS_ABI3:
+        return True
+    ext = "pyd" if WIN else "abi3.so"
+    bin = SP_DIR / f"pyproject_fmt/_lib.{ext}"
+    return do(["abi3audit", bin, "-s", "-v", f"--assume-minimum-abi3={python_min}"])
+
+
 if __name__ == "__main__":
     sys.exit(
+        check_abi(sys.argv[1])
         # run the tests
-        call([*COV, *RUN, *PYTEST, *K])
+        or do([*COV, *RUN, *PYTEST, *K])
         # maybe run coverage
-        or call([*COV, *REPORT])
+        or do([*COV, *REPORT])
     )
